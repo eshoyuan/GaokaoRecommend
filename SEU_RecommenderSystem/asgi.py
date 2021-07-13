43d1c3a3423bf0dfaad30d28c308918835483324
吴泳良 <50854709@qq.com>
2021-07-13 21:54:46 +0800

    create a new project of django

---
+++ SEU_RecommenderSystem/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for SEU_RecommenderSystem project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SEU_RecommenderSystem.settings')
+
+application = get_asgi_application()
