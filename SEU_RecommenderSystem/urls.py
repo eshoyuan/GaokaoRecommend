e8fafe22950cb96d890fb67922a0c32a76aaddc5
Syy <2283572267@qq.com>
2021-07-14 22:40:30 +0800

    添加数据库功能测试url

--- SEU_RecommenderSystem/urls.py
+++ SEU_RecommenderSystem/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from . import testdb
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('recommend/', include(('recommend.urls', 'recommend'), namespace='recommend')),
+    path('testdb/', testdb.testdb)
 ]
