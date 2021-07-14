2b89cd0ad86ae422a48bdb0774fd7a905b25df3a
eshoyuan <eshoyuan@gmail.com>
2021-07-14 11:52:26 +0800

    add welcome.html to test

--- recommend/views.py
+++ recommend/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-
-
+from django.shortcuts import render
 # 增加了一个视图，用作测试
 def index(request):
     return HttpResponse("Hello, world. This index is for recommender.")
+def welcome(request):
+    return render(request, "welcome.html")
