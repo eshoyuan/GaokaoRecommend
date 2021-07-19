49c4d0804bbcac1783d526bcc5203bdb773ddb7a
郭文 <2602583886@qq.com>
2021-07-19 08:13:50 +0000

    update SEU_RecommenderSystem/settings.py.

--- SEU_RecommenderSystem/settings.py
+++ SEU_RecommenderSystem/settings.py
@@ -71,14 +71,28 @@ WSGI_APPLICATION = 'SEU_RecommenderSystem.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+       #'NAME':'recommend_collegeapplication',
+        'USER':'admin',
+        'PASSWORD':'58119',
+        'HOST':'127.0.0.1',
 
+        'OPTIONS': {
+            'timeout': 20,
+        }
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
