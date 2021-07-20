2e0df80929d75438861bdfddde9cbafd8aad5d9d
吴泳良 <501271374@qq.com>
2021-07-20 15:32:51 +0800

    更正脚本

--- read_from_csv.py
+++ read_from_csv.py
@@ -7,7 +7,7 @@ from recommend.models import CollegeApplication
 # 物化生政史地，提取选课要求
 # 0表示不作要求，1表示有要求；对于'要求'，0表示多选一，1表示多选多
 def parser(subject):
-    subject_request = {'物': 0, '化': 0, '生': 0, '政': 0, '史': 0, '地': 0, '要求':0}
+    subject_request = {'物': 0, '化': 0, '生': 0, '政': 0, '史': 0, '地': 0, '要求': 0}
     if subject == '不限':
         return subject_request
     elif len(subject) == 1:
@@ -42,11 +42,11 @@ with open(file_path, 'r', encoding='UTF-8') as f:
                                                     rank_int=row[5],
                                                     is_985=row[7],
                                                     is_211=row[8],
-                                                    Phy=subject_request[0],
-                                                    Che=subject_request[1],
-                                                    Bio=subject_request[2],
-                                                    Pol=subject_request[3],
-                                                    His=subject_request[4],
-                                                    Geo=subject_request[5],
-                                                    request=subject_request[6],
+                                                    Phy=subject_request['物'],
+                                                    Che=subject_request['化'],
+                                                    Bio=subject_request['生'],
+                                                    Pol=subject_request['政'],
+                                                    His=subject_request['史'],
+                                                    Geo=subject_request['地'],
+                                                    request=subject_request['要求'],
                                                     id=reader.line_num - 1)
