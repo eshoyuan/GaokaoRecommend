d32fdcfd9f118f50c27a082e6d66afcb5136092f
syy <2283572267@qq.com>
2021-07-23 17:00:21 +0800

    按专业要求筛选

--- recommend/views.py
+++ recommend/views.py
@@ -1,3 +1,4 @@
+import numpy as np
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
@@ -33,44 +34,8 @@ def welcome_output(request):
     return HttpResponse(response)
 
 
-
 def results(request):
-    chosen_list = [
-        request.GET.get('cbox_Phy'),
-        request.GET.get('cbox_Che'),
-        request.GET.get('cbox_Bio'),
-        request.GET.get('cbox_Pol'),
-        request.GET.get('cbox_His'),
-        request.GET.get('cbox_Geo')
-                   ]
-    Range = request.GET.get("input_range")
-    results_list_0 = CollegeApplication.objects.filter(request=0).filter(rank_int__gt=Range).order_by('rank_int')
-    results_list_1 = CollegeApplication.objects.filter(request=1).filter(rank_int__gt=Range).order_by('rank_int')
-    result = []
-    for i in results_list_0:
-        request_list = [i.Phy,
-                        i.Che,
-                        i.Bio,
-                        i.Pol,
-                        i.His,
-                        i.Geo,
-                        ]
-        temp = chosen_list - request_list
-        if -1 not in temp:
-            result.append(i)
-    for i in results_list_1:
-        request_list = [i.Phy,
-                        i.Che,
-                        i.Bio,
-                        i.Pol,
-                        i.His,
-                        i.Geo,
-                        ]
-        temp = chosen_list - request_list
-        c1 = Counter(request_list)['1']
-        c2 = Counter(temp)['-1']
-        if c2 <= c1-1:
-            result.append(i)
+    result = CollegeApplication.objects.all()
     template = loader.get_template('recommend/results.html')
     context = {
         'list': result,
@@ -81,9 +46,48 @@ def results(request):
 # 新页面
 def new_page(request):
     Range = request.GET.get("input_range")
-    result = CollegeApplication.objects.all().filter(rank_int__gt=Range).order_by('rank_int')
-    # for i in result:
-    #     path=
+    get_list = [
+        request.GET.get('cbox_Phy'),
+        request.GET.get('cbox_Che'),
+        request.GET.get('cbox_Bio'),
+        request.GET.get('cbox_Pol'),
+        request.GET.get('cbox_His'),
+        request.GET.get('cbox_Geo')
+    ]
+    chosen_list = []
+    for i in get_list:
+        if i == '1':
+            chosen_list.append(1)
+        else:
+            chosen_list.append(0)
+    results_list_0 = CollegeApplication.objects.filter(request=0).filter(rank_int__gt=Range).order_by('rank_int')
+    results_list_1 = CollegeApplication.objects.filter(request=1).filter(rank_int__gt=Range).order_by('rank_int')
+    result = []
+    for i in results_list_0:
+        request_list = [int(i.Phy),
+                        int(i.Che),
+                        int(i.Bio),
+                        int(i.Pol),
+                        int(i.His),
+                        int(i.Geo),
+                        ]
+        temp = [chosen_list[i] - request_list[i] for i in range(len(chosen_list))]
+        if -1 not in temp:
+            result.append(i)
+    for i in results_list_1:
+        request_list = [int(i.Phy),
+                        int(i.Che),
+                        int(i.Bio),
+                        int(i.Pol),
+                        int(i.His),
+                        int(i.Geo),
+                        ]
+        temp = [chosen_list[i] - request_list[i] for i in range(len(chosen_list))]
+        c1 = Counter(request_list)[1]
+        c2 = Counter(temp)[-1]
+        print(c1, c2)
+        if c2 <= c1 - 1:
+            result.append(i)
     return render(request, 'recommend/return.html', {  # 由table.html修改为results.html
         'collegeapplication': result
     })
